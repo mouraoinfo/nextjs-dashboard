63760a4c17dc844cd3861e03dc76aa1f8a55f346
mouraoinfo <dev@mourao.info>
2024-10-21 19:00:00 -0300

    Update page title to reflect current chapter with emoji

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
 
-          <h1 className="text-blue-500">I'm at Master and from Chapter 2</h1>
+          <h1 className="text-blue-500">I'm at Master and from Chapter 2 😊👌</h1>
 
           <Link
             href="/login"
